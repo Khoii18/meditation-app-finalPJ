20a88683f3a53289933aa8f1dd11e345a5bc04ea
Khoiimen <KhoiNAGCH221106@fpt.edu.vn>
2026-04-24 12:10:27 +0700

    Fix middleware name error

--- server/routes/recommendation.route.js
+++ server/routes/recommendation.route.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { getRecommendations, updateRecommendation, deleteRecommendation } from "../controllers/recommendation.controller.js";
-import { verifyToken, isAdmin } from "../middleware/auth.js";
+import { verifyToken, verifyAdmin } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/", getRecommendations);
-router.post("/", verifyToken, isAdmin, updateRecommendation);
-router.delete("/:id", verifyToken, isAdmin, deleteRecommendation);
+router.post("/", verifyToken, verifyAdmin, updateRecommendation);
+router.delete("/:id", verifyToken, verifyAdmin, deleteRecommendation);
 
 export default router;
