863aaae4c8f2ccd69732162534d61a5b06a3b45e
Khoiimen <KhoiNAGCH221106@fpt.edu.vn>
2026-04-29 02:45:41 +0700

    Permanently remove test-node.js

--- test-node.js
+++
@@ -1,6 +0,0 @@
-const { exec } = require('child_process');
-
-exec('node app.js', { cwd: 'c:\\Users\\ADMIN\\Desktop\\finalPJ\\meditation-app-finalPJ\\server' }, (err, stdout, stderr) => {
-  console.log("STDOUT:", stdout);
-  console.error("STDERR:", stderr);
-});
